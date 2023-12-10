44319c948d2dc3677c72e3575dd179cb8a1a9365
Erwan <erwanclou@gmail.com>
2023-12-11 00:14:33 +0100

    Add apparition effect to bento

--- components/section/bento.tsx
+++ components/section/bento.tsx
@@ -4,6 +4,10 @@ import Title from "../text/title"
 import Image from "next/image"
 import Link from "next/link"
 import { PrimaryLink } from "../button/button"
+import gsap from "gsap"
+import { useEffect, useRef } from "react"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+gsap.registerPlugin(ScrollTrigger)
 
 export default function BentoSection() {
     
@@ -16,6 +20,26 @@ export default function BentoSection() {
         {name: 'Umanao', link: 'https://umanao.com/', image: '/clients/logo_umanao.png'},
         {name: 'https://www.flowlinetechnologies.com/', link: '/prestations/developpement-logiciel', image: '/clients/FLTechnoSCC_logo.png'},
     ]
+
+    // Apparition des éléments de la grille
+
+    useEffect(() => {
+      const gridItems = document.querySelectorAll(".row-span-1")
+      for (let i = 0; i < gridItems.length; i++) {
+        gsap.to(gridItems[i], {
+          scrollTrigger: {
+            trigger: "#bento",
+            start: "top 80%",
+            toggleActions: "play none none none",
+          },
+          opacity: 1,
+          y: 0,
+          x: 0,
+          duration: 0.4,
+          delay: i * 0.2,
+        })        
+      }
+    }), []
 
     return (
         <Section id="bento" className="flex justify-center gap-8 lg:justify-around flex-col bg-[#0f0f0f]">
@@ -33,7 +57,14 @@ export default function BentoSection() {
           {clients.map((client, i) => (
             <div
               key={i}
-              className={`row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-1 md:p-10 ${
+              {...(i === 0 ? { style: { transform: 'translateX(-100px)' } } : '' )}
+              {...(i === 1 ? { style: { transform: 'translateY(-100px)' } } : '' )}
+              {...(i === 2 ? { style: { transform: 'translateX(100px)' } } : '' )}
+              {...(i === 3 ? { style: { transform: 'translateX(-100px)' } } : '' )}
+              {...(i === 4 ? { style: { transform: 'translateX(100px)' } } : '' )}
+              {...(i === 5 ? { style: { transform: 'translateY(100px)' } } : '' )}
+              {...(i === 6 ? { style: { transform: 'translateX(100px)' } } : '' )}
+              className={`row-span-1 rounded-lg border-2 border-slate-400/10 bg-neutral-100 p-1 opacity-0 md:p-10 ${
                 i === 3 || i === 6 ? "col-span-2" : ""
               }`}
             >
